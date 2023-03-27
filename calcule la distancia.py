f6a150865f774575501a8839c0ec0db3db8107f5
arielriva5 <129062014+arielriva5@users.noreply.github.com>
2023-03-27 16:40:52 +0200

    m/s

---
+++ calcule la distancia.py
@@ -0,0 +1,7 @@
+print("CALCULE SU RECORRIDO CON SU VELOCIDAD Y TIEMPO ")
+print("------------------------------------------------")
+print(" ingrese la velocidad y el tiempo")
+V= float (input ("velocidad")) 
+T=float(input("tiempo"))
+D=V*T
+print("el recorido fue",D)
