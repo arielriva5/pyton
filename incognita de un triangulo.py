8a01830960450d6cfc52ea05bd7eab15a6f7f812
arielriva5 <129062014+arielriva5@users.noreply.github.com>
2023-04-10 10:29:40 -0300

    incógnita de X e Y en un triangulo

---
+++ incognita de un triangulo.py
@@ -0,0 +1,9 @@
+print (" recta de un triangulo ")
+print("ingrese coordenadas del Punto a: ")
+ax=float(input("ax: "))
+ay=float(input("ay: "))
+print("ingrese coordenadas del Punto b: ")
+bx=float(input("bx: "))
+by=float(input("by: "))
+d=((ax-bx)**2+(ay-by)**2)**0.5
+print("el resultado es: ", d)
