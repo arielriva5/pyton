315de3c570570fb757d52143d3dd05e63681de11
arielriva5 <129062014+arielriva5@users.noreply.github.com>
2023-04-10 10:15:29 -0300

    partidos del club ABC

---
+++ club ABC.py
@@ -0,0 +1,7 @@
+g=int(input("partidos ganados ",))
+p=int(input("partidos perdidos ",))
+e=int(input("partidos epatados ",))
+j=g+p+e
+v=(g*3)-p
+print("partidos jugados por el club ABC: ",j)
+print("victorias del club ABC: ",v)
