faa83d8a62fe879f09e47e298aa03039fac1ecf3
arielriva5 <129062014+arielriva5@users.noreply.github.com>
2023-04-10 10:14:45 -0300

    punto de preguntas

---
+++ puntajes.py
@@ -0,0 +1,9 @@
+c=int(input("respuestas correctas ",))
+i=int(input("respuestas incorrectas ",))
+b=int(input("respuestas en blanco ",))
+pre=c+i+b
+p=(c*3)-i
+
+print(" la cantidad de preguntas son: ",pre)
+print(" su puntaje es de: ",p)
+
